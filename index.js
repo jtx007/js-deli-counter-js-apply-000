f77ac3014b462b90947680d7d4b5a1b0367c1499
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-27 05:49:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function nowServing(katzDeliLine) {
     return first
   }
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'The line is currently empty.'
+  }
+}
