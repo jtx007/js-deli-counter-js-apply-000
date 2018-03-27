3dac786d055deb8f67477ce81514ef728461f24d
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-27 05:10:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine[name] - 1} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
